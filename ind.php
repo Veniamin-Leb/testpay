d2b2f5a965769bb4844d2b7d7210f80d4bbc1645
Veniamin-Leb <62715894+Veniamin-Leb@users.noreply.github.com>
2021-12-24 15:11:58 +0300

    Update ind.php

--- ind.php
+++ ind.php
@@ -3,19 +3,4 @@
 
     $client = new Client();
     $client->setAuth('<867315>', '<test_j0Oso70m9JPGhk2V3Dpp0fkHIOai4rBSbRubX7sYQEM>');
-    $payment = $client->createPayment(
-        array(
-            'amount' => array(
-                'value' => 100.0,
-                'currency' => 'RUB',
-            ),
-            'confirmation' => array(
-                'type' => 'redirect',
-                'return_url' => 'https://www.merchant-website.com/return_url',
-            ),
-            'capture' => true,
-            'description' => 'Заказ №1',
-        ),
-        uniqid('', true)
-    );
 ?>
